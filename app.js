f4d040b14a615e5280063e9d7beac2483c0fd8f9
b0bthcdrz <bob.appworkz@tutanota.de>
2024-02-08 18:03:26 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+
+        const app = document.getElementById('app');
+        const header = document.createElement('h1');
+        const text = 'Learn React';
+        const headerContent = document.createTextNode(text);
+        header.appendChild(headerContent);
+        app.appendChild(header);
